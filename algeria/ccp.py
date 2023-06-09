9308e870ebdd6d30e62da36b0824093204f7b9db
mouh2020 <badimohammed2019@gmail.com>
2023-06-09 14:16:42 +0100

    add rip and rip's cle

--- algeria/ccp.py
+++ algeria/ccp.py
@@ -1,7 +1,8 @@
 class CCP : 
-    def __init__(self,ccp) : 
+    def __init__(self,ccp : str) : 
         self.ccp = ccp
     def get_cle(self) :
+        # Return the cle of the account
         x = self.ccp.zfill(10)
         values = list(x)
         cc = 0
@@ -11,3 +12,22 @@ class CCP :
             z -= 1
         ccc = str(cc % 100).zfill(2)
         return ccc
+    def get_rip(self,only_cle=None) : 
+        # Return the rip of the account including the first 8 digits 00799999
+        self.ccp = int(self.ccp)
+        if ((self.ccp * 100) % 97) + 85 > 97:
+            x = 97 - (((self.ccp * 100) % 97) + 85 - 97)
+            if x < 10:
+                return "00799999"+ str(self.ccp).zfill(10) + str(x) if not(only_cle) else str(x)
+            else:
+                return "00799999"+ str(self.ccp).zfill(10) + str(x) if not(only_cle) else str(x)
+        else:
+            y = 97 - (((self.ccp * 100) % 97) + 85)
+            if y < 10:
+                return "00799999"+ str(self.ccp).zfill(10) + str(y) if not(only_cle) else str(y)
+            else:
+                return "00799999"+ str(self.ccp).zfill(10) + str(y)if not(only_cle) else str(y)
+    def get_rip_cle(self) : 
+        # Return only the cle of the rip 
+        return self.get_rip(only_cle=True)
+        
