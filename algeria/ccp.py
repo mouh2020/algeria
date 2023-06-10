eed9554a470beb907e9e09bed47fd6fc3107c510
mouh2020 <badimohammed2019@gmail.com>
2023-06-10 14:46:34 +0100

    remove duplicate returns and move the whole clé calculation to the private method

--- algeria/ccp.py
+++ algeria/ccp.py
@@ -14,23 +14,20 @@ class CCP:
         ccc = str(cc % 100).zfill(2)
         return ccc
 
-    def __calculate_rip(self, x):
+    def __calculate_rip(self) :
         # Calculate the rip based on the given value x
-        if x < 10:
-            return "00799999" + str(self.ccp).zfill(10) + str(x)
-        else:
-            return "00799999" + str(self.ccp).zfill(10) + str(x)
-
-    def get_rip(self, only_cle=None):
-        # Calculate and return the rip of the account
         self.ccp = int(self.ccp)
         remainder = (self.ccp * 100) % 97
         if remainder + 85 > 97:
             x = 97 - (remainder + 85 - 97)
         else:
             x = 97 - (remainder + 85)
-            
-        return self.__calculate_rip(x) if not only_cle else str(x)
+        return "00799999" + str(self.ccp).zfill(10) + str(x)
+
+    def get_rip(self, only_cle=None):
+        # Calculate and return the rip of the account
+        rip = self.__calculate_rip()
+        return rip if not only_cle else rip[-2:]
   
     def get_rip_cle(self) : 
         # Return only the cle of the rip 
